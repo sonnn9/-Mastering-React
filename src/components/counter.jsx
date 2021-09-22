74a6b9b2de2df92ff554ad4ccaff7c598ed8465b
sonnguyen <sng198x@gmail.com>
2021-09-22 08:50:05 +0700

    call direct function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,11 +6,10 @@ class Counter extends  Component {
     };
     
     render() {
-        let classes = this.getBadgeClasses();
 
         return (
             <div>
-                <span className={classes}>{this.formatCount()}</span>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
