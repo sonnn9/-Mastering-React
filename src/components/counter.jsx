36da26e86107378d30cd5b210afe312e8a0477a9
sonnguyen <sng198x@gmail.com>
2021-09-22 08:22:38 +0700

    setting attributes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,10 +5,16 @@ class Counter extends  Component {
         count: 0
     };
 
+    styles = {
+        fontSize: 50,
+        fontWeight: "bold"
+    };
+
     render() {
         return (
             <div>
-                <h1>Hello World { this.formatCount() }</h1>
+                <h1 style={ this.styles }>Hello World { this.formatCount() }</h1>
+                <h1 style={{ fontSize: 30 }}>Hello World { this.formatCount() }</h1>
                 <button>Increase</button>
             </div>
         );
