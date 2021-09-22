cafe1efb97b1b92c3d36cb704cf2a59aab4bd08d
sonnguyen <sng198x@gmail.com>
2021-09-22 15:17:31 +0700

    handling events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,18 +6,16 @@ class Counter extends  Component {
         tags: ["tag1","tag2","tag3"]
     };
 
-    renderTags() {
-        if(this.state.tags.length === 0) return <p>There are no tags!</p>;
-
-        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+    handleIncrement() {
+        console.log('Increment Clicked', this);
     }
 
     render() {
 
         return (
             <div>
-                {this.state.tags.length === 0 && "Please create a new tag!"}
-                {this.renderTags()}
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
     }
