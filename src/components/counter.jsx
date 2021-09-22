24404ae8ab70be4c6cf3359f75385d861f3bbe60
sonnguyen <sng198x@gmail.com>
2021-09-22 15:25:38 +0700

    binding via arrow function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,12 +6,12 @@ class Counter extends  Component {
         tags: ["tag1","tag2","tag3"]
     };
 
-    constructor() {
-        super();
-        this.handleIncrement = this.handleIncrement.bind(this);
-    }
-
-    handleIncrement() {
+    // constructor() {
+    //     super();
+    //     this.handleIncrement = this.handleIncrement.bind(this);
+    // }
+    
+    handleIncrement = () => {
         console.log('Increment Clicked', this);
     }
 
