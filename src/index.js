15edfeaaab33a291d1306ceb97c5f5a30a2c53be
sonnguyen <sng198x@gmail.com>
2021-09-22 16:40:05 +0700

    setting up the vidly project

--- src/index.js
+++ src/index.js
@@ -4,14 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.css";
-import Counter from "./components/counter";
+import "font-awesome/css/font-awesome.css"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Counter />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
