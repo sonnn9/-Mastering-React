700aad13c4bf38434ccd1ede9be198315072e46a
sonnguyen <sng198x@gmail.com>
2021-09-25 22:39:49 +0700

    lifting the state up

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,48 +2,19 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends  Component {
-    state = {
-        counters: [
-            { id: 1, value: 4},
-            { id: 2, value: 0},
-            { id: 3, value: 0},
-            { id: 4, value: 0},
-        ]
-    };
 
-    handleIncrement = counter => {
-        const counters =[...this.state.counters];
-        const index = counters.indexOf(counter);
-        counters[index] = {...counter};
-        counters[index].value++;
-        this.setState({counters})
-    }
-
-    handleDelete = counterId => {
-        const counters = this.state.counters.filter(c => c.id !== counterId);
-        this.setState({counters: counters})
-    }
-
-    handleReset = () => {
-        const counters = this.state.counters.map(c => {
-           c.value = 0;
-           return c;
-        });
-        this.setState({counters})
-    }
 
     render() {
         return (
         <div>
-            <button className="btn btn-primary" onClick={this.handleReset}>Reset</button>
+            <button className="btn btn-primary" onClick={this.props.onReset}>Reset</button>
 
-            {this.state.counters.map(counter =>
+            {this.props.counters.map(counter =>
                 <Counter
                     key={counter.id}
-                    onDelete={this.handleDelete}
-                    onIncrement={this.handleIncrement}
+                    onDelete={this.props.onDelete}
+                    onIncrement={this.props.onIncrement}
                     counter={counter}
-                    id={counter.id}
                 />
             )}
         </div>
