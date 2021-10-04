502ae5a911e96772a5df6da6e57ee8eae96685f7
sonnguyen <sng198x@gmail.com>
2021-10-04 22:39:47 +0700

    24_mounting_phase

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 
 // Stateless Functional Component
 const NavBar = ({ totalCounters}) => {
+    console.log("narbar - rendered");
+
     return (
         <nav className="navbar navbar-light bg-light">
             <a className="navbar-brand" href="#">
